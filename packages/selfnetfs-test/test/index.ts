443329eb83c542df9fd5334c4c6231f4accb0950
Keith <vorble@cochleoid.com>
2021-04-12 08:54:47 -0400

    First working test of server.

--- packages/selfnetfs-test/test/index.ts
+++ packages/selfnetfs-test/test/index.ts
@@ -3,14 +3,13 @@ import * as uuid from 'uuid';
 import { equal } from 'assert';
 import { Http } from 'selfnetfs';
 import { Memory, PasswordModuleNull } from 'selfnetfs-memory';
-import { Server, Persist, OwnerPool } from 'selfnetfs-server';
+import { Server, PersistMemory } from 'selfnetfs-server';
 
-const persist = new Persist('./database');
+const persist = new PersistMemory();
+persist.bootstrap('owner', 'user', 'password');
 const server = new Server({
   port: 4001,
-  owners: new OwnerPool<Memory>(persist, () => {
-    return new Memory(uuid.v4, new PasswordModuleNull());
-  }),
+  persist: persist,
 });
 
 function abit() {
@@ -25,7 +24,8 @@ describe('testing', function() {
   it('should log in', async function() {
     server.listen();
     await abit(); // So there's time for the listener to start
-    const api = new Http('http://127.0.0.1:4001/test');
-    await api.login({ name: 'test', password: 'password' });
+    const api = new Http('http://127.0.0.1:4001/owner');
+    await api.login({ name: 'user', password: 'password' });
+    server.destroy();
   });
 });
