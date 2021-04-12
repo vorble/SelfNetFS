ad3ef6830886058d0afb10b285989d525dcde40f
Keith <vorble@cochleoid.com>
2021-04-12 10:52:22 -0400

    selfnetfs-test: Using pluggable logger to squelch logging during test.

--- packages/selfnetfs-test/test/index.ts
+++ packages/selfnetfs-test/test/index.ts
@@ -5,25 +5,22 @@ import { Http } from 'selfnetfs';
 import { Memory, PasswordModuleNull } from 'selfnetfs-memory';
 import { Server, PersistMemory } from 'selfnetfs-server';
 
+export class LoggerNull {
+  error(...args: any[]): void {}
+  log(...args: any[]): void {}
+}
+
 const persist = new PersistMemory();
 persist.bootstrap('owner', 'user', 'password');
 const server = new Server({
   port: 4001,
   persist: persist,
+  logger: new LoggerNull(),
 });
-
-function abit() {
-  return new Promise<void>((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 1000);
-  });
-}
 
 describe('testing', function() {
   it('should log in', async function() {
-    server.listen();
-    await abit(); // So there's time for the listener to start
+    await server.listen();
     const api = new Http('http://127.0.0.1:4001/owner');
     await api.login({ name: 'user', password: 'password' });
     server.destroy();
